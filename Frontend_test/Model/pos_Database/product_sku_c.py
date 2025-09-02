3d1aadfa806130f47e6925575928ebe58c2965d2
phattpde140043 <pinn.b18pct@gmail.com>
2025-09-02 17:01:47 +0700

    update function

--- Frontend_test/Model/pos_Database/product_sku_c.py
+++ Frontend_test/Model/pos_Database/product_sku_c.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 from common import execute_query, pos_dbname, user, password, host, port
+import pandas as pd
+
 
 class ProductSKU:
     def __init__(self, sku_id, sku, product_id, color, size, price, 
@@ -17,23 +19,77 @@ class ProductSKU:
         self.product = None
         self.order_items = []
 
+    @staticmethod
     def getall():
-        query = "Select * from products_sku"
+        query = """
+            SELECT sku_id, sku, product_id, color, size, price, created_at
+            FROM products_sku WHERE deleted_at IS NULL
+        """
+        rows = execute_query(pos_dbname, user, password, host, port, query)
+
+        # Ép kết quả thành list[ProductSKU]
+        skus = [
+            ProductSKU(
+                row[0], row[1], row[2], row[3],
+                row[4], row[5], row[6]
+            )
+            for row in rows
+        ]
+        return skus
+    
+    @staticmethod
+    def getSkuByProductId(ProductId):
+        query = f"""
+            SELECT sku_id, sku, product_id, color, size, price, created_at
+            FROM products_sku WHERE deleted_at IS NULL AND product_id = '{ProductId}'
+        """
+        rows = execute_query(pos_dbname, user, password, host, port, query)
+
+        # Ép kết quả thành list[ProductSKU]
+        skus = [
+            ProductSKU(
+                row[0], row[1], row[2], row[3],
+                row[4], row[5], row[6]
+            )
+            for row in rows
+        ]
+        return skus
+
+    @staticmethod
+    def toPandas(sku_list):
+        """Chuyển list[ProductSKU] thành pandas.DataFrame"""
+        data = [{
+            "sku_id": sku.sku_id,
+            "sku": sku.sku,
+            "product_id": sku.product_id,
+            "color": sku.color,
+            "size": sku.size,
+            "price": sku.price,
+            "created_at": sku.created_at
+        } for sku in sku_list]
+
+        return pd.DataFrame(data)
+
+    @staticmethod
+    def insert_SKU(p_sku,p_product_id,p_price,p_color,p_size):
+        query= f"Select insert_sku('{p_sku}','{p_product_id}',{p_price},'{p_color}','{p_size}')"
         return execute_query(pos_dbname, user, password, host, port, query)
 
+    @staticmethod
+    def update_SKU(p_sku_id,p_sku,p_price,p_color,p_size):
+        query= f"Select update_sku({p_sku_id},'{p_sku}','{p_color}','{p_size}',{p_price})"
+        return execute_query(pos_dbname, user, password, host, port, query)
 
-    # Business logic
-    def mark_deleted(self):
-        """Đánh dấu SKU đã bị xóa (soft delete)."""
-        self.deleted_at = datetime.now()
+    @staticmethod
+    def soft_deleted(p_sku_id):
+        query= f"Select soft_delete_sku({p_sku_id})"
+        return execute_query(pos_dbname, user, password, host, port, query)
 
-    def get_total_sold(self):
-        """Tính tổng số lượng đã bán từ order_items."""
-        return sum(item.quantity for item in self.order_items)
+    @staticmethod
+    def searchByRange(p_min, p_max):
+        query= f"Select search_skus_by_price({p_min},{p_max})"
+        return execute_query(pos_dbname, user, password, host, port, query)
 
-    def get_total_revenue(self):
-        """Tính tổng doanh thu từ order_items."""
-        return sum(item.quantity * item.unit_price for item in self.order_items)
 
     def __repr__(self):
         return (f"<ProductSKU(id={self.sku_id}, sku={self.sku}, "
