e6a228c99ceea09a614867ba91c81b58d8da1c8f
phattpde140043 <pinn.b18pct@gmail.com>
2025-10-06 00:05:53 +0700

    Revert "commit 2"
    This reverts commit eebb7f6bee1febabecd487396d435f10e8bdb9a5.

---
+++ Backend_test/Model/pos_Database/payment_types_c.py
@@ -0,0 +1,19 @@
+from common import execute_query, pos_dbname, user, password, host, port
+
+class PaymentType:
+    def __init__(self, payment_type_id, name):
+        self.payment_type_id = payment_type_id
+        self.name = name
+
+    # Business logic
+    def rename(self, new_name):
+        """Đổi tên phương thức thanh toán"""
+        self.name = new_name
+
+    def getall():
+        query = "Select * from payment_types"
+        return execute_query(pos_dbname, user, password, host, port, query)
+
+
+    def __repr__(self):
+        return f"<PaymentType(id={self.payment_type_id}, name={self.name})>"
